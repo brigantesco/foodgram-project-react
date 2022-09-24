59b530173ad9c4e29510a0c289d3c02337adf406
Andrey Yastrebov <geeksogen@gmail.com>
2022-09-24 12:22:50 +0300

    flake8v1

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,7 +1,6 @@
 from django.db.models import Sum
 from django.http.response import HttpResponse
 from django.shortcuts import get_object_or_404
-
 from django_filters.rest_framework import DjangoFilterBackend
 from djoser.serializers import SetPasswordSerializer
 from recipes.models import (Favorites, Follow, Ingredient, Recipe,
--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -1,8 +1,7 @@
 from django.conf import settings
+from django.contrib.auth import get_user_model
 from django.core.validators import MinValueValidator
 from django.db import models
-
-from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
@@ -69,7 +68,7 @@ class Recipe(models.Model):
         related_name='recipes',
         verbose_name='Автор рецепта',
         help_text='Автор рецепта',
-        )
+    )
     name = models.CharField(
         max_length=200,
         db_index=True,
