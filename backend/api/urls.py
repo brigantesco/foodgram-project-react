ead1d3015819aeac3b3fc18da4437ea0de435b50
Andrey Yastrebov <geeksogen@gmail.com>
2022-09-27 12:33:22 +0300

    some_change1

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -6,14 +6,14 @@ from .views import (CustomUserViewSet, IngredientViewSet, RecipeViewSet,
 
 router = DefaultRouter()
 
-router.register('users', CustomUserViewSet)
-router.register('tags', TagViewSet)
 router.register('recipes', RecipeViewSet, basename='recipes')
 router.register('ingredients', IngredientViewSet)
+router.register('users', CustomUserViewSet)
+router.register('tags', TagViewSet)
 
 urlpatterns = [
-    path('users/<int:pk>/', UserDetail.as_view(), name='user_detail'),
     path('', include(router.urls)),
+    path('users/<int:pk>/', UserDetail.as_view(), name='user_detail'),
     path('', include('djoser.urls')),
     path('auth/', include('djoser.urls.authtoken')),
 ]
